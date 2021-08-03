0d7e2b823cbebead583fa0a956c59874e295b895
Saj Panchal <sajpanchal@gmail.com>
2021-08-03 10:24:59 -0400

    Project Initialized

---
+++ InstaFilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstaFilter
+//
+//  Created by saj panchal on 2021-08-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
