e574c1c89dc62179ff1567a5bd035b19d4067014
Saj Panchal <sajpanchal@gmail.com>
2021-08-05 11:56:15 -0400

    added Coordinator class to handle image selection event and attach image to image property.

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -12,7 +12,7 @@ import CoreImage.CIFilterBuiltins
 struct ContentView: View {
     @State var image: Image?
     @State var showingImagePicker = false
-    
+    @State var inputImage: UIImage?
     var body: some View {
         VStack {
             image?
@@ -25,38 +25,15 @@ struct ContentView: View {
                 Text("Select Image")
             })
         }
-        .sheet(isPresented: $showingImagePicker, content: {
-            ImagePicker()
+        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage, content: {
+            ImagePicker(image: self.$inputImage)
         })
     }
     func loadImage() {
-        guard let inputImage = UIImage(named: "image") else {
+        guard let inputImage = inputImage else {
             return
         }
-        let beginImage = CIImage(image: inputImage)
-        let context = CIContext()
-        guard let currentFilter = CIFilter(name: "CITwirlDistortion") else {
-            return
-        }
-        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-        //currentFilter.inputImage = beginImage
-        
-        currentFilter.setValue(2000, forKey: kCIInputRadiusKey)
-        //currentFilter.radius = 200
-        
-        currentFilter.setValue(CIVector(x: inputImage.size.width / 2, y: inputImage.size.height / 2), forKey: kCIInputCenterKey)
-        
-        //get CIImage from currentFilter.
-        guard let outputImage = currentFilter.outputImage else {
-            return
-        }
-        // get CGImage from CIImage.
-        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-            // convert that to a UIImage
-            let uiImage = UIImage(cgImage: cgimg)
-            // convert UIImage to Image
-            image = Image(uiImage: uiImage)
-        }
+        image = Image(uiImage: inputImage)
     }
 }
 
--- InstaFilter/ImagePicker.swift
+++ InstaFilter/ImagePicker.swift
@@ -8,8 +8,28 @@
 import Foundation
 import SwiftUI
 struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+    // method is being called itself when image is selected.
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    // instantiated when image is selected in image picker.
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: ImagePicker
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            parent.presentationMode.wrappedValue.dismiss()  
+        }
+    }
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
         return picker
     }
     
