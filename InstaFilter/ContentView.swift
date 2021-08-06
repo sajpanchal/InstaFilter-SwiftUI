a3ab25a23fd787ed203618635227ef36389f4a43
Saj Panchal <sajpanchal@gmail.com>
2021-08-06 14:15:55 -0400

    added code to filter the loaded image and dynamically apply the intensity of filter.

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -14,8 +14,19 @@ struct ContentView: View {
     @State var filterIntensity = 0.5
     @State var showingImagePicker = false
     @State var inputImage: UIImage?
+    @State var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     var body: some View {
+        let intensity = Binding<Double> (
+            get: {
+                self.filterIntensity
+            },
+            set: {
+                self.filterIntensity = $0
+                self.applyProcessing()
+            }
+        )
         VStack {
             ZStack {
                 Rectangle()
@@ -35,7 +46,7 @@ struct ContentView: View {
             }
             HStack {
                 Text("Intensity")
-                Slider(value: self.$filterIntensity)
+                Slider(value: intensity)
             }
             .padding(.vertical)
             HStack {
@@ -59,7 +70,19 @@ struct ContentView: View {
         guard let inputImage = inputImage else {
             return
         }
-        image = Image(uiImage: inputImage)
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        guard let outputImage = currentFilter.outputImage else {
+            return
+        }
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
     
 }
