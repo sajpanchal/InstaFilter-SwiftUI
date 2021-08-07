fe98bb0adcb36516446ed3f728f4337ece521c49
Saj Panchal <sajpanchal@gmail.com>
2021-08-07 12:57:21 -0400

    added code to pop up actionSheet with different filters and apply intensity to it.

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -14,7 +14,8 @@ struct ContentView: View {
     @State var filterIntensity = 0.5
     @State var showingImagePicker = false
     @State var inputImage: UIImage?
-    @State var currentFilter = CIFilter.sepiaTone()
+    @State var currentFilter: CIFilter = CIFilter.sepiaTone()
+    @State var showingFilterSheet = false
     let context = CIContext()
     
     var body: some View {
@@ -51,7 +52,7 @@ struct ContentView: View {
             .padding(.vertical)
             HStack {
                 Button("Change Filter") {
-                    //change filter
+                    self.showingFilterSheet = true
                 }
                 Spacer()
                 Button("Save") {
@@ -64,7 +65,39 @@ struct ContentView: View {
         .sheet(isPresented: $showingImagePicker, onDismiss: loadImage, content: {
             ImagePicker(image: self.$inputImage)
         })
+        .actionSheet(isPresented: $showingFilterSheet) {
+           
+            return ActionSheet(title: Text("Select a filter"), buttons: [
+                            .default(Text("Crystallize")) {
+                                self.setFilter(CIFilter.crystallize())
+                            },
+                            .default(Text("Edges")) {
+                                self.setFilter(CIFilter.edges())
+                            },
+                            .default(Text("Gaussian Blur")) {
+                                self.setFilter(CIFilter.gaussianBlur())
+                            },
+                            .default(Text("Pixellate")) {
+                                self.setFilter(CIFilter.pixellate())
+                            },
+                            .default(Text("Sepia Tone")) {
+                                self.setFilter(CIFilter.sepiaTone())
+                            },
+                            .default(Text("Unsharp Mask")) {
+                                self.setFilter(CIFilter.unsharpMask())
+                            },
+                            .default(Text("Vignette")) {
+                                self.setFilter(CIFilter.vignette())
+                            },
+                            .cancel()
+                            
+            ])
+        }
        
+    }
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
     }
     func loadImage() {
         guard let inputImage = inputImage else {
@@ -75,7 +108,16 @@ struct ContentView: View {
         applyProcessing()
     }
     func applyProcessing() {
-        currentFilter.intensity = Float(filterIntensity)
+        let inputKeys = currentFilter.inputKeys
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+        }
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)
+        }
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)
+        }
         guard let outputImage = currentFilter.outputImage else {
             return
         }
